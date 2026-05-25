cd059f466188749614812e73cd11f875e2faff1f
Jonny Salazar Hernandez <mordecaiusm922@gmail.com>
2026-05-24 23:04:40 -0500

    add login function

---
+++ vulnerable.py
@@ -0,0 +1,9 @@
+﻿import os
+import pickle
+
+def login(request):
+    user = request.args.get("user")
+    db.execute("SELECT * FROM users WHERE name = '" + user + "'")
+    secret = "hardcoded_secret_key_12345"
+    data = pickle.loads(request.data)
+    return data
